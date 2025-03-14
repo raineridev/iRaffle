9e7753fe0c97075c8e54d9db9a1da09b3bdcb50d
Marcelo Raineri <marcelo@gutlz.com>
2025-03-14 21:11:54 +0000

    test

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['shadcn-nuxt'],
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
 })
